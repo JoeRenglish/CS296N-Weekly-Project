b2f435cf2c248c8fa43499b49fe654ac6472cd78
Joseph English <music.jre@gmail.com>
2025-01-14 00:47:50 -0800

    initial commit

---
+++ StoryCreator/StoryCreator/Controllers/CreateController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StoryCreator.Controllers
+{
+    public class CreateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CharacterCreator()
+        {
+            return View();
+        }
+
+        public IActionResult StoryCreator()
+        {
+            return View();
+        }
+    }
+}
